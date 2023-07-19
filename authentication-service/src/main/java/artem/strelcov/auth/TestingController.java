7a29461cee4e58d02d9834dfd7da68dff22cb8ab
SophisticatedThings <sophisticatedthings@mail.ru>
2023-07-19 17:24:59 +0300

    Added login page

--- authentication-service/src/main/java/artem/strelcov/auth/TestingController.java
+++ authentication-service/src/main/java/artem/strelcov/auth/TestingController.java
@@ -11,6 +11,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestingController {
     @GetMapping
     public String test(){
-        return "Allright";
+        return "registration.html";
     }
 }
